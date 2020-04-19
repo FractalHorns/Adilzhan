eea8a033de434908340779df7e4f3952e51eee2e
SamigulovAF <49686465+FractalHorns@users.noreply.github.com>
2020-04-19 22:17:49 +0600

    Add files via upload

---
+++ kotlin/RStudentList.kt
@@ -0,0 +1,28 @@
+import data.Student
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.events.Event
+import react.*
+import react.dom.li
+import react.dom.ol
+
+interface RStudentListProps : RProps {
+    var students: Array<Student>
+    var present: Array<Boolean>
+    var onClick:  (Int) -> (Event) -> Unit
+}
+
+val RFstudentlist =
+    functionalComponent<RStudentListProps> { props ->
+        props.students.mapIndexed { index, student ->
+            li {
+                rstudent(student, props.present[index], props.onClick(index))
+            }
+        }
+    }
+
+fun RBuilder.rstudentlist(students: Array<Student>, present: Array<Boolean>, onClick:(Int) -> (Event) -> Unit) =
+    child(RFstudentlist) {
+        attrs.students = students
+        attrs.present = present
+        attrs.onClick = onClick
+    }
